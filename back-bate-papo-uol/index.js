5da905e9b042f054c2a2b0893de23a9ffe5c16e4
okitauehara <marcosokita@live.com>
2021-10-10 16:43:32 -0300

    Registrar participante finalizado

--- back-bate-papo-uol/index.js
+++ back-bate-papo-uol/index.js
@@ -6,4 +6,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let participants = [];
+let messages = [];
+
+app.post('/participants', (req, res) => {
+    const username = req.body;
+    const alreadyExists = participants.find(user => user.name === username.name);
+    if (username.name.length === 0) {
+        res.status(400)
+    } else if (alreadyExists) {
+        res.status(401)
+    } else {
+        const participant = {
+            name: username.name,
+            lastStatus: Date.now()
+        }
+
+        const loginMessage = {
+            from: participant.name,
+            to: 'Todos',
+            text: 'entra na sala...', 
+            type: 'status', 
+            time: getLocalTime()
+        }
+        
+        participants.push(participant);
+        messages.push(loginMessage);
+        res.status(200)
+    }
+})
+
 app.listen(4000);
+
+function getLocalTime() {
+    return new Date().toLocaleString().split(' ')[1]
+}
